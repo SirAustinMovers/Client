24c8143317019785b37ca189663b12fc0f941d57
Sir Austin <sir@siraustinmovers.com>
2019-07-28 13:29:40 -0500

    Latest form submission

--- mobile/www/js/controllers.js
+++ mobile/www/js/controllers.js
@@ -18,9 +18,23 @@ angular.module('your_app_name.controllers', ['ionic','ion-fab-button'])
   };
 })
 
-.controller('FormCtrl', function($rootScope, $scope, $ionicActionSheet, $http, $ionicModal, $state,  WORDPRESS_API_URL, AuthService) {
+.controller('FormCtrl', function($rootScope, $scope, $ionicActionSheet, $q, $http, $ionicModal, $state,  WORDPRESS_API_URL, AuthService) {
+
     
-    var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}};
+    
+    var data = {
+ title: 'Title',
+ excerpt: 'Excerpt',
+ content: 'Content',
+ status: 'publish'
+ };
+ // the important bit, make a request to the server to create a new post
+ // The Authentication header will be added to the request automatically by our Interceptor service
+ $http.post('http://adam:123456@localhost/wp-json/wp/v2/posts', data).error(function (error) {
+ $q.deferred.reject(error);
+ }).success(function (data) {
+ $q.deferred.resolve(data);
+ });
     
     /*
     jQuery.ajax({
@@ -42,11 +56,56 @@ angular.module('your_app_name.controllers', ['ionic','ion-fab-button'])
    }
 });
 */
-    
+ /*   
   $scope.user = AuthService.getUser();
     console.log($scope.user);
+    */
     
-     $scope.postRequest = function() {
+    
+    
+    /*
+          $scope.master = {};
+
+      $scope.update = function(user) {
+        $scope.master = angular.copy(user);
+
+    console.log($scope.master);
+    var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}};          
+           jQuery.ajax({
+   url: 'http://127.0.0.1/wp-json/wp/v2/posts',
+   method: 'POST',
+   crossDomain: true,
+   beforeSend: function ( xhr ) {
+       xhr.setRequestHeader( 'Authorization', 'Basic ' + Base64.encode( 'adam:123456' ) );
+   },
+      data: {
+        title: user.title,
+        content: user.content,
+          status: 'publish'
+      },
+        
+   success: function( data, txtStatus, xhr ) {
+       console.log( data );
+       console.log( xhr.status );
+   }
+});
+          
+          
+                    
+          localStorage.setItem('newPost', JSON.stringify($scope.master));
+          console.log($scope.master);
+      };
+
+      $scope.reset = function() {
+        $scope.user = angular.copy($scope.master);
+      };
+
+      $scope.reset();
+    
+    */
+     /*
+ $scope.postRequest = function() {
+    
      jQuery.ajax({
    url: 'http://127.0.0.1/wp-json/wp/v2/posts',
    method: 'POST',
@@ -65,9 +124,56 @@ angular.module('your_app_name.controllers', ['ionic','ion-fab-button'])
        console.log( xhr.status );
        location.reload();
    }
+}); */
+     
+     
+     /*
+          $scope.master = {};
+
+      $scope.update = function(user) {
+        $scope.master = angular.copy(user);
+          
+          $scope.user();
+          console.log($scope.user);
+          
+           jQuery.ajax({
+   url: 'http://127.0.0.1/wp-json/wp/v2/posts',
+   method: 'POST',
+   crossDomain: true,
+   beforeSend: function ( xhr ) {
+       xhr.setRequestHeader( 'Authorization', 'Basic ' + Base64.encode( 'adam:123456' ) );
+   },
+      data: {
+        title: user.title,
+        content: user.content,
+          status: 'publish'
+      },
+        
+   success: function( data, txtStatus, xhr ) {
+       console.log( data );
+       console.log( xhr.status );
+   }
 });
-    
+          
+          
+          
+          
+          
+          
+          localStorage.setItem('newPost', JSON.stringify($scope.master));
+          console.log($scope.master);
+      };
+
+      $scope.reset = function() {
+        $scope.user = angular.copy($scope.master);
+      };
+
+      $scope.reset();
+     
     };
+    */
+    
+    
     /*
       $scope.postRequest = function() {
           var data = ""
